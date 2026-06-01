f341f017562712474ea79061f1f351b35d7a6b0f
Aristeu-Junior <aristeujr.dev@hotmail.com>
2026-06-01 01:52:14 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-// module.exports = app;
+const http = require("http");
+const { handleRequest } = require("./routes/router");
 
-// const http = require("http");
-// const { handleRequest } = require("./routes/router");
+const PORT = Number(process.env.PORT) || 3000;
 
-// const PORT = Number(process.env.PORT) || 3000;
-
-// http.createServer(handleRequest).listen(PORT, () => {
-//   console.log(`Just Do Eat rodando em http://localhost:${PORT}`);
-
-// Condição para rodar o app.listen apenas no seu PC, e não no Vercel
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(3000, () => {
-        console.log('Servidor rodando localmente na porta 3000');
-    });
+if (require.main === module) {
+  http.createServer(handleRequest).listen(PORT, () => {
+    console.log(`Just Do Eat rodando em http://localhost:${PORT}`);
+  });
 }
 
-// Essa exportação é obrigatória para o Vercel entender seu backend
-module.exports = app;
-});
+module.exports = handleRequest;
